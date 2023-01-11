93d8b053daf22063a9efe1ea072df7159d202c2b
hungtrn75 <hungtrn75@gmail.com>
2023-01-11 15:00:11 +0700

    update

--- example/lib/full_map.dart
+++ example/lib/full_map.dart
@@ -54,6 +54,10 @@ class FullMapState extends State<FullMap> {
           onMapCreated: _onMapCreated,
           initialCameraPosition: const CameraPosition(target: LatLng(0.0, 0.0)),
           onStyleLoadedCallback: _onStyleLoadedCallback,
+          trackCameraPosition: true,
+          onCameraMove: (position) {
+            print("onCameraMove $position");
+          },
         ));
   }
 }
